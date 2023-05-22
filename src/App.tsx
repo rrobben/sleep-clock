b5838d16bb9e28786408fea31ef5ab9be5b87e39
rrobben <roope.poyry1@gmail.com>
2023-05-22 17:15:51 +0300

    Adjust colors and and add icons

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import "./App.css";
 import moment from "moment";
 import { useInterval } from "./hooks/useInterval";
 import { useScreenLock } from "./hooks/useScreenLock";
+import BedtimeIcon from "@mui/icons-material/Bedtime";
+import LightModeIcon from "@mui/icons-material/LightMode";
 
 const SLEEPS = [
     ["12:45", "15:00"],
@@ -12,8 +14,9 @@ const SLEEPS = [
 const FORMAT = "HH:mm";
 
 function App() {
+    const mode = new URLSearchParams(window.location.search).get("mode");
     const [time, setTime] = useState<moment.Moment>(moment());
-    const [sleep, setSleep] = useState<boolean>(false);
+    const [sleep, setSleep] = useState<boolean>(mode === "sleep");
 
     const updateTime = () => {
         setTime(moment());
@@ -22,30 +25,33 @@ function App() {
     useInterval(updateTime, 5000);
 
     useEffect(() => {
-        setSleep(
-            SLEEPS.some((s) => {
-                const now = moment();
-                const sleepStart = moment(s[0], FORMAT);
-                const sleepEnd = moment(s[1], FORMAT);
+        if (!mode) {
+            setSleep(
+                SLEEPS.some((s) => {
+                    const now = moment();
+                    const sleepStart = moment(s[0], FORMAT);
+                    const sleepEnd = moment(s[1], FORMAT);
 
-                if (sleepStart.isAfter(sleepEnd)) {
-                    if (now.isAfter(sleepStart) && now.isAfter(sleepEnd)) {
-                        sleepEnd.add(1, "day");
-                    } else {
-                        sleepStart.subtract(1, "day");
+                    if (sleepStart.isAfter(sleepEnd)) {
+                        if (now.isAfter(sleepStart) && now.isAfter(sleepEnd)) {
+                            sleepEnd.add(1, "day");
+                        } else {
+                            sleepStart.subtract(1, "day");
+                        }
                     }
-                }
 
-                return now.isAfter(sleepStart) && now.isBefore(sleepEnd);
-            })
-        );
+                    return now.isAfter(sleepStart) && now.isBefore(sleepEnd);
+                })
+            );
+        }
     }, [time]);
 
     useScreenLock();
 
     return (
         <div className="App">
-            <header className="App-header">
+            <header className={`App-header ${sleep ? "sleeping" : ""}`}>
+                {sleep ? <BedtimeIcon /> : <LightModeIcon />}
                 <p className={`clock ${sleep ? "sleeping" : ""}`}>{time.format(FORMAT)}</p>
             </header>
         </div>
